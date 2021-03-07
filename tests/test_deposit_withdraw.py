b1a684169ac8914f641a3559a49a25b41895b591
Michael Egorov <michwill@gmail.com>
2021-03-07 22:11:57 +0100

    Simple withdraw test

--- tests/test_deposit_withdraw.py
+++ tests/test_deposit_withdraw.py
@@ -1,3 +1,4 @@
+import brownie
 from brownie.test import given, strategy
 from hypothesis import settings  # noqa
 from .conftest import INITIAL_PRICES
@@ -27,17 +28,36 @@ def test_1st_deposit_and_last_withdraw(crypto_swap, coins, token, accounts):
 
 @given(values=strategy('uint256[3]', min_value=10**16, max_value=10**6 * 10**18))
 @settings(max_examples=MAX_SAMPLES)
-def test_second_deposit(crypto_swap_with_deposit, token, coins, accounts, values, crypto_math):
+def test_second_deposit(crypto_swap_with_deposit, token, coins, accounts, values):
     user = accounts[1]
-    if all(v == 0 for v in values):
-        return
     amounts = [v * 10**18 // p for v, p in zip(values, [10**18] + INITIAL_PRICES)]
     for c, v in zip(coins, amounts):
         c._mint_for_testing(user, v)
 
     calculated = crypto_swap_with_deposit.calc_token_amount(amounts, True)
     measured = token.balanceOf(user)
-    crypto_swap_with_deposit.add_liquidity(amounts, 0, {'from': user})
+    crypto_swap_with_deposit.add_liquidity(amounts, int(calculated * 0.999), {'from': user})
     measured = token.balanceOf(user) - measured
 
     assert calculated == measured
+
+
+@given(token_amount=strategy('uint256', min_value=10**12, max_value=4000 * 10**18))  # supply is 2400 * 1e18
+@settings(max_examples=MAX_SAMPLES)
+def test_immediate_withdraw(crypto_swap_with_deposit, token, coins, accounts, token_amount):
+    user = accounts[1]
+
+    f = token_amount / token.totalSupply()
+    if f <= 1:
+        expected = [int(f * crypto_swap_with_deposit.balances(i)) for i in range(3)]
+        measured = [c.balanceOf(user) for c in coins]
+        crypto_swap_with_deposit.remove_liquidity(
+                token_amount,
+                [int(0.999 * e) for e in expected],
+                {'from': user})
+        measured = [c.balanceOf(user) - m for c, m in zip(coins, measured)]
+        for e, m in zip(expected, measured):
+            assert abs(e - m) / e < 1e-3
+    else:
+        with brownie.reverts():
+            crypto_swap_with_deposit.remove_liquidity(token_amount, [0] * 3, {'from': user})
