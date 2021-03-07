e0789411d55e86e7ec36646a9dee5725f02e50c0
Michael Egorov <michwill@gmail.com>
2021-03-07 14:06:11 +0100

    1st deposit and last withdraw

--- tests/test_deposit_withdraw.py
+++ tests/test_deposit_withdraw.py
@@ -1,7 +1,7 @@
 from .conftest import INITIAL_PRICES
 
 
-def test_deposit_and_withdraw(crypto_swap, coins, token, accounts, crypto_math):
+def test_1st_deposit_and_last_withdraw(crypto_swap, coins, token, accounts, crypto_math):
     user = accounts[1]
     quantities = [10**36 // p for p in [10**18] + INITIAL_PRICES]  # $3 worth
     for coin, q in zip(coins, quantities):
@@ -11,5 +11,11 @@ def test_deposit_and_withdraw(crypto_swap, coins, token, accounts, crypto_math):
     # Very first deposit
     crypto_swap.add_liquidity(quantities, 0, {'from': user})
 
-    assert token.balanceOf(user) == token.totalSupply() > 0
+    token_balance = token.balanceOf(user)
+    assert token_balance == token.totalSupply() > 0
     assert abs(crypto_swap.get_virtual_price() / 1e18 - 1) < 1e-3
+
+    # Empty the contract
+    crypto_swap.remove_liquidity(token_balance, [0] * 3, {'from': user})
+
+    assert token.balanceOf(user) == token.totalSupply() == 0
