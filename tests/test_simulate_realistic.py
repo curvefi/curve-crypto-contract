255ac676fada9e3405192c91fea99705041e506b
Michael Egorov <michwill@gmail.com>
2021-04-05 14:56:15 +0200

    Realistic parameters for simulation test (for gas measurements)

---
+++ tests/test_simulate_realistic.py
@@ -0,0 +1,15 @@
+from brownie.test import strategy
+from .test_simulate import StatefulSimulation
+
+MAX_SAMPLES = 10
+STEP_COUNT = 20
+
+
+class Realistic(StatefulSimulation):
+    exchange_amount_in = strategy('uint256', min_value=100 * 10**18, max_value=5000 * 10**18)
+    sleep_time = strategy('uint256', max_value=200)
+
+
+def test_sim(crypto_swap, token, chain, accounts, coins, state_machine):
+    state_machine(Realistic, chain, accounts, coins, crypto_swap, token,
+                  settings={'max_examples': MAX_SAMPLES, 'stateful_step_count': STEP_COUNT})
