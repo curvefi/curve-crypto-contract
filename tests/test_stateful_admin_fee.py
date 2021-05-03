73c87b05271ec7b2c05763c30455068bcb95dd93
Michael Egorov <michwill@gmail.com>
2021-05-03 12:51:03 +0200

    Disable health checks for test of admin fees

--- tests/test_stateful_admin_fee.py
+++ tests/test_stateful_admin_fee.py
@@ -56,5 +56,7 @@ class StatefulAdmin(StatefulBase):
 
 
 def test_admin(crypto_swap, token, chain, accounts, coins, state_machine):
+    from hypothesis._settings import HealthCheck
+
     state_machine(StatefulAdmin, chain, accounts, coins, crypto_swap, token,
-                  settings={'max_examples': MAX_SAMPLES, 'stateful_step_count': STEP_COUNT})
+                  settings={'max_examples': MAX_SAMPLES, 'stateful_step_count': STEP_COUNT, 'suppress_health_check': HealthCheck.all()})
