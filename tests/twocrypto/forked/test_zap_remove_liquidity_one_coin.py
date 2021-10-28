e1e149831d18d8fd62a43757b46fda2d8c428039
Michael Egorov <michwill@gmail.com>
2021-10-28 13:22:26 +0200

    calc_withdraw_one_coin

--- tests/twocrypto/forked/test_zap_remove_liquidity_one_coin.py
+++ tests/twocrypto/forked/test_zap_remove_liquidity_one_coin.py
@@ -14,20 +14,28 @@ def lp_token_amount(alice, crypto_zap, token):
 
 @pytest.mark.parametrize("idx", range(4))
 def test_remove_one_coin(alice, crypto_zap, underlying_coins, lp_token_amount, idx):
+    calc_amount = crypto_zap.calc_withdraw_one_coin(lp_token_amount, idx)
+    amount = underlying_coins[idx].balanceOf(alice)
     crypto_zap.remove_liquidity_one_coin(lp_token_amount, idx, 0, {"from": alice})
+    amount = underlying_coins[idx].balanceOf(alice) - amount
 
-    assert underlying_coins[idx].balanceOf(alice) > INITIAL_AMOUNTS[idx]
+    assert amount > INITIAL_AMOUNTS[idx]
+    assert abs(amount - calc_amount) / amount < 0.01
 
 
 @pytest.mark.parametrize("idx,scale", it.product(range(4), [0.3, 0.6, 0.9]))
 def test_remove_one_coin_percentage(
     alice, crypto_zap, underlying_coins, lp_token_amount, idx, scale
 ):
+    calc_amount = crypto_zap.calc_withdraw_one_coin(int(lp_token_amount * scale), idx)
+    amount = underlying_coins[idx].balanceOf(alice)
     crypto_zap.remove_liquidity_one_coin(
         int(lp_token_amount * scale), idx, 0, {"from": alice}
     )
+    amount = underlying_coins[idx].balanceOf(alice) - amount
 
-    assert underlying_coins[idx].balanceOf(alice) > INITIAL_AMOUNTS[idx] * scale / 5
+    assert amount > INITIAL_AMOUNTS[idx] * scale / 5
+    assert abs(amount - calc_amount) / amount < 0.01
 
 
 @pytest.mark.parametrize("idx", range(4))
