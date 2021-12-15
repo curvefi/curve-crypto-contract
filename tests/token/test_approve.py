97cc52c42e1382ca1d554450f6d95068da8a1fce
Edward Amor <edward.amor3@gmail.com>
2021-12-15 15:00:27 +0000

    test: eip 1271 support

--- tests/token/test_approve.py
+++ tests/token/test_approve.py
@@ -113,3 +113,37 @@ def test_permit(accounts, bob, chain, token, web3):
     assert tx.events["Approval"].values() == [alice.address, bob, 2 ** 256 - 1]
     assert token.nonces(alice) == 1
 
+
+def test_permit_contract(accounts, bob, chain, token, web3):
+
+    src = """
+@view
+@external
+def isValidSignature(_hash: bytes32, _sig: Bytes[65]) -> bytes32:
+    return 0x1626ba7e00000000000000000000000000000000000000000000000000000000
+    """
+    mock_contract = brownie.compile_source(src, vyper_version="0.3.1").Vyper.deploy({"from": bob})
+    alice = accounts.add("0x416b8a7d9290502f5661da81f0cf43893e3d19cb9aea3c426cfb36e8186e9c09")
+
+    class Permit(EIP712Message):
+        # EIP-712 Domain Fields
+        _name_: "string" = token.name()
+        _version_: "string" = token.version()
+        _chainId_: "uint256" = chain.id
+        _verifyingContract_: "address" = token.address
+
+        # EIP-2612 Data Fields
+        owner: "address"
+        spender: "address"
+        value: "uint256"
+        nonce: "uint256"
+        deadline: "uint256" = 2 ** 256 - 1
+
+    permit = Permit(owner=alice.address, spender=bob.address, value=2 ** 256 - 1, nonce=0)
+    sig = alice.sign_message(permit)
+
+    tx = token.permit(mock_contract, bob, 2 ** 256 - 1, 2 ** 256 - 1, sig.v, sig.r, sig.s)
+
+    # make sure this is hit when owner is a contract
+    assert tx.subcalls[0]["function"] == "isValidSignature(bytes32,bytes)"
+
